2b25a6dde7ebc46ed3ab54c4ea62b850122862e2
sufailbadushat <sufailbadushat@gamil.com>
2022-12-07 16:35:40 +0530

    search course using course title from database

--- src/main/java/com/nest/course_backend/dao/CourseDao.java
+++ src/main/java/com/nest/course_backend/dao/CourseDao.java
@@ -1,8 +1,13 @@
 package com.nest.course_backend.dao;
 
 import com.nest.course_backend.model.Courses;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CourseDao extends CrudRepository<Courses,Integer> {
-
+    @Query(value = "SELECT `id`, `date`, `description`, `duration`, `title`, `venue` FROM `courses` WHERE `title`= :title" , nativeQuery = true)
+    List<Courses> SearchCourses(@Param("title") String title);
 }
